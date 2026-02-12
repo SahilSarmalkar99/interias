7e8b12e0959023b8bb5a6bbb2dff7cf31ea732dc
sahilsarmalkar <sahilgaming667@gmail.com>
2026-02-12 18:20:54 +0530

    changing bg

--- frontend/src/components/Home/HeroImage.jsx
+++ frontend/src/components/Home/HeroImage.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import img from "../../assets/display/bg.png"
+import img from "../../assets/display/bgg.png"
 
 const HeroImage = () => {
   const imgRef = useRef(null);
