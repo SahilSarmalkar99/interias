cfb09d753bd57a55d3f3830a3b5ff66496179fb8
sahilsarmalkar <sahilgaming667@gmail.com>
2026-02-14 18:35:54 +0530

    added new logo

--- frontend/src/components/Home/Clients.jsx
+++ frontend/src/components/Home/Clients.jsx
@@ -1,31 +1,30 @@
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import img1 from "../../assets/logos/arrow.png";
-import img2 from "../../assets/logos/AxisSecurities.jpg";
-import img3 from "../../assets/logos/collection.png";
-import img4 from "../../assets/logos/Guess.png";
-import img5 from "../../assets/logos/HDBFinancialServices.png";
-import img6 from "../../assets/logos/HDFCLife.jpg";
-import img7 from "../../assets/logos/Hindalco.webp";
-import img8 from "../../assets/logos/KanungoGroup.webp";
-import img9 from "../../assets/logos/Kipling.png";
-import img10 from "../../assets/logos/Lee.png";
-import img11 from "../../assets/logos/Levis.jpg";
-import img12 from "../../assets/logos/LodhaGroup.png";
-import img13 from "../../assets/logos/MotilalOswal.png";
-import img14 from "../../assets/logos/Nautica.png";
-import img15 from "../../assets/logos/PeterEngland.png";
-import img16 from "../../assets/logos/puma.png";
-import img17 from "../../assets/logos/sephora.jpg";
-import img18 from "../../assets/logos/TommyHilfiger.png";
-import img19 from "../../assets/logos/US_PoloAssn.png";
-import img20 from "../../assets/logos/VanHeusen.png";
+import img1 from "../../assets/logos/logo1.png"
+import img2 from "../../assets/logos/logo2.png"
+import img3 from "../../assets/logos/logo3.png"
+import img4 from "../../assets/logos/logo4.png"
+import img5 from "../../assets/logos/logo5.png"
+import img6 from "../../assets/logos/logo6.png"
+import img7 from "../../assets/logos/logo7.png"
+import img8 from "../../assets/logos/logo8.png"
+import img9 from "../../assets/logos/logo9.png"
+import img10 from "../../assets/logos/logo10.png"
+import img11 from "../../assets/logos/logo11.png"
+import img12 from "../../assets/logos/logo12.png"
+import img13 from "../../assets/logos/logo13.png"
+import img14 from "../../assets/logos/logo14.png"
+import img15 from "../../assets/logos/logo15.png"
+import img16 from "../../assets/logos/logo16.png"
+import img17 from "../../assets/logos/logo17.png"
+import img18 from "../../assets/logos/logo18.png"
+import img19 from "../../assets/logos/logo19.png"
 import useRevealAnimation from "../../hooks/useRevealAnimation";
 
 const logos = [
   img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
-  img11, img12, img13, img14, img15, img16, img17, img18, img19, img20
+  img11, img12, img13, img14, img15, img16, img17, img18, img19
 ];
 
 const Clients = () => {
@@ -65,7 +64,7 @@ const Clients = () => {
               key={i}
               src={logo}
               alt="Client Logo"
-              className="h-20 shrink-0"
+              className="h-15 md:h-20 shrink-0"
               draggable={false}
             />
           ))}
